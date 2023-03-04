d413483eadde5a5b83b8aee1f519e10aa6972a0d
Pablock <pablo.sanchez@gmail.com>
2023-03-03 23:32:54 -0500

    Primer commit

---
+++ src/assets/fake-data/data-about.js
@@ -0,0 +1,20 @@
+const dataAbout = [
+    {
+        id: 1,
+        title: 'Equidad y transparencia',
+        desc: 'Con la ayuda de tecnologías blockchain y contratos inteligentes, la web3 puede facilitar una distribución más justa y transparente de las regalías de la música.',
+        class: 'active'
+    },
+    {
+        id: 2,
+        title: 'Gestion de Regalias',
+        desc: 'Gracias a esta tecnología, los artistas pueden recibir un pago justo por su trabajo, los fans pueden apoyar a sus artistas favoritos de manera directa y transparente, y los derechos de autor se gestionan de manera más eficiente.',
+    },
+    {
+        id: 3,
+        title: 'Usage rights included',
+        desc: 'Blockchain puede ayudar a garantizar la autenticidad de la música y la mercancía de un artista. Los artistas pueden usar blockchain para registrar la propiedad de sus obras y crear tokens no fungibles (NFT) que representen la propiedad única de una pieza de música o un artículo de mercancía.',
+    },
+]
+
+export default dataAbout;
---
+++ src/assets/fake-data/data-slider.js
@@ -0,0 +1,45 @@
+import bgImg1 from '../images/background/bg-1.jpg';
+import bgImg2 from '../images/background/bg-3.jpg';
+import bgImg3 from '../images/background/bg-4.jpg';
+import img1 from '../images/common/itemslider.png';
+
+const dataSlider = [
+    {
+        id: 1,
+        title: 'Colecciones musicales y experiencias tokenizadas',
+        desc : 'utilizamos la tecnología blockchain para tokenizar cada set y brindar la oportunidad de crear nuevos modelos de negocio digitales y un sinfín de beneficios para la industria de la música.',
+        bgImg: bgImg1,
+        img : img1
+    },
+    {
+        id: 2,
+        title: 'Musica en formato NFT al alcance de todos',
+        desc : 'la web3 tiene el potencial de transformar significativamente la forma en que se crea, se distribuye y se consume la música, ofreciendo beneficios tanto para los artistas como para los fans.',
+        bgImg: bgImg2,
+        classAction: 'two'
+    },
+    {
+        id: 3,
+        title: 'Radio Online',
+        desc : 'La web3 ofrece oportunidades para la creación de nuevos modelos de negocio, como el uso de tokens no fungibles (NFT) para la venta de música y el uso de criptomonedas para la compra de boletos para conciertos.',
+        bgImg: bgImg3,
+        classAction: 'three'
+    },
+    {
+        id: 4,
+        title: 'CYbox nft collectionS for everyone',
+        desc : 'Nulla ornare sagittis placerat nunc sit tempus enim. Accumsan pellentesque ipsum felis tristique at proin vel turpis.',
+        bgImg: bgImg1,
+        img : img1
+    },
+    {
+        id: 5,
+        title: 'CYbox nft collectionS for everyone',
+        desc : 'Nulla ornare sagittis placerat nunc sit tempus enim. Accumsan pellentesque ipsum felis tristique at proin vel turpis.',
+        bgImg: bgImg2,
+        classAction: 'two'
+    },
+
+]
+
+export default dataSlider;
